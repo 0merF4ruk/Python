dcd817c1fcb2207c2986dc1620837ae373bdfae3
Omer Faruk OKUMUS <113258370+0merF4ruk@users.noreply.github.com>
2022-11-25 17:43:53 +0300

    25.11.22.17.45
    16 py dosyası yüklenmeye çalışılıyor
    
    Signed-off-by: Omer Faruk OKUMUS <113258370+0merF4ruk@users.noreply.github.com>

---
+++ if3.py
@@ -0,0 +1,6 @@
+username = input("Kullanıcı Adı:")
+password = input("Parola:")
+if username == "admin" and password == "admin":
+    print("Giriş başarılı")
+else:
+    print("Giriş başarısız!")
---
+++ while2.py
@@ -0,0 +1,6 @@
+sayi = int(input("Kaça kadar çift sayıların yazılacağını belirtin:"))
+sayici = 0
+while sayi > sayici:
+    if sayici % 2 == 0:
+        print(sayici)
+    sayici += 1
